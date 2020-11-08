17f91e8196adc5f74c33812c7f1524cba2648d64
Sharon-liu-go <innovate72095@gmail.com>
2020-11-08 17:04:46 +0800

    fear:add user login feature via passport

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const bodyParser = require('body-parser')
 require('./config/mongoose')
 const methodOverride = require('method-override')
 const session = require('express-session')
+const usePassport = require('./config/passport')
 const routes = require('./routes')
 
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -18,6 +19,8 @@ app.use(session({
   resave: false,
   saveUninitialized: true
 }))
+usePassport(app)
+
 app.use(routes)
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
